d18a3f2ad1afe30b67c60d78360975dee2d954d1
hazedrops <hazedrops@hotmail.com>
2021-01-11 21:42:51 -0800

    Github profile search app using the Github API - JavaScript

---
+++ script.js
@@ -0,0 +1,93 @@
+const APIURL = 'http://api.github.com/users/'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username)
+
+    createUserCard(data)
+    getRepos(username)
+  } catch(err) {
+    if(err.response.status == 404) {
+      createErrorCard('No profile with this username')
+    }    
+  } 
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + '/repos?sort=created')
+
+    addReposToCard(data)
+  } catch(err) {
+      createErrorCard('Problem fetching repos')
+      console.log(err)
+  } 
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+    <div class="card">
+      <div>
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+      </div>
+      <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+
+        <ul>
+          <li>${user.followers} <strong>Followers</strong></li>
+          <li>${user.following} <strong>Followings</strong></li>
+          <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos"></div>
+      </div>
+    </div>
+  `
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class="card">
+      <h1>${msg}</h1>
+    </div>
+  `
+
+  main.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos')
+
+  repos
+    .slice(0,10) // Only get the first 10 repos
+    .forEach(repo => {
+      const repoLinkEl = document.createElement('a')
+
+      repoLinkEl.classList.add('repo')
+      repoLinkEl.href = repo.html_url
+      repoLinkEl.target = '_blank'  // Opens in a new window
+      repoLinkEl.innerText = repo.name
+
+      reposEl.appendChild(repoLinkEl)
+  })
+
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const user = search.value
+
+  if(user) {
+    getUser(user)
+
+    search.value = ''
+  }
+}) 
+
